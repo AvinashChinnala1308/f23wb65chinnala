f1ad81909608363844d1e8035f38a5f65401df52
AvinashChinnala1308 <avinashchinnala1325@gmail.com>
2023-10-31 22:59:12 -0500

    modified class and added bonus

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,39 +1,41 @@
-var express = require('express');
-var router = express.Router();
 
-var organizations = [
-    {
-      organizationName: 'Bosch Global Software Technologies',
-      location: 'Hyderabad, India',
-      industry: 'Technology',
-      numberOfEmployees: 3000,
-      revenue: 50000000,
-      website: 'https://www.bosch-softwaretechnologies.com/en/our-company/about-us/',
-    },
-    {
-      organizationName: 'Wipro',
-      location: 'Whitefields, Inida',
-      industry: 'Technology',
-      numberOfEmployees: 50000,
-      revenue: 7000000,
-      website: 'https://www.wipro.com/',
-    },
-    {
-        organizationName: 'Apps Associates',
-        location: 'Uppal, Inida',
-        industry: 'Technology',
-        numberOfEmployees: 5000,
-        revenue: 7000000,
-        website: 'https://appsassociates.com/',
-      }
+const express = require('express');
+const router = express.Router();
 
-
-
+const organizations = [
+  { 
+    organizationName: 'Bosch Global Software Technologies',
+    location: 'Hyderabad, India',
+    industry: 'Technology',
+    numberOfEmployees: 3000,
+    revenue: 50000000,
+    website: 'https://www.bosch-softwaretechnologies.com/en/our-company/about-us/'},
+  { 
+    organizationName: 'Wipro',
+    location: 'Whitefields, India',
+    industry: 'Technology',
+    numberOfEmployees: 50000,
+    revenue: 7000000, 
+    website: 'https://www.wipro.com/' },
+  { 
+    organizationName: 'Apps Associates',
+    location: 'Uppal, Inida',
+    industry: 'Technology',
+    numberOfEmployees: 5000,
+    revenue: 7000000,
+    website: 'https://appsassociates.com/'},
+  { 
+    organizationName: 'Epam',
+    location: 'Inida',
+    industry: 'MI',
+    numberOfEmployees: 900,
+    revenue: 3000000,
+    website: 'https://www.epam.com/'}
+  
 ];
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('organizations', { title: 'Search Results' });
+router.get('/', (req, res) => {   
+    res.render('organizations', { title: 'Search Results - Organizations', results: organizations });
   });
-
+  
   module.exports = router;
