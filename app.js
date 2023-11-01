77da5889df0f3c5ff813847c1dff84df5d9f7052
AvinashChinnala1308 <avinashchinnala1325@gmail.com>
2023-10-31 22:01:21 -0500

    choose is added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var organizationsRouter = require('./routes/organizations');
 const boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/organizations', organizationsRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 
 
